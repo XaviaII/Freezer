6adabf897e90d33e6753b47f306f9118617a7bc0
Daniel Poetscher <daniel_poetscher@gmx.at>
2022-03-14 17:46:19 +0100

    vid 7 - Premake

---
+++ premake5.lua
@@ -0,0 +1,103 @@
+workspace "Freezer"
+	architecture "x64"
+
+	configurations {
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+
+
+
+outputdir = "%{cfg.buildcfg}_%{cfg.system}_%{cfg.architecture}"
+
+project "Freezer"
+	location "Freezer"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin_int/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs {
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++20"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines {
+			"FREEZER_PLATFORM_WINDOWS",
+			"FREEZER_BUILD_DLL"
+		}
+
+		postbuildcommands {
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Testing")
+		}
+
+	filter "configurations:Debug"
+		defines "FREEZER_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "FREEZER_RELEASE"
+		optimize "On"
+
+	filter "configurations:DIST"
+		defines "FREEZER_DIST"
+		optimize "On"
+
+
+
+
+
+project "Testing"
+	location "Testing"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin_int/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs {
+		"Freezer/vendor/spdlog/include",
+		"Freezer/src"
+	}
+
+	links {
+		"Freezer"
+	}
+
+	filter "system:windows"
+		cppdialect "C++20"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines {
+			"FREEZER_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "FREEZER_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "FREEZER_RELEASE"
+		optimize "On"
+
+	filter "configurations:DIST"
+		defines "FREEZER_DIST"
+		optimize "On"
