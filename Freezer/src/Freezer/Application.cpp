399824c8291aa0b2d0cb097f39d396113ba2b230
Daniel Poetscher <daniel_poetscher@gmx.at>
2022-03-17 12:04:00 +0100

    vid 12 - Window Events

--- Freezer/src/Freezer/Application.cpp
+++ Freezer/src/Freezer/Application.cpp
@@ -1,18 +1,25 @@
 #include "Freezerpch.h"
 #include "Application.h"
 
-#include "Freezer/Events/ApplicationEvent.h"
-
 #include "GLFW/glfw3.h"
 
 namespace Freezer {
 
+#define BIND_EVENT_FN(x) std::bind(&x, this, std::placeholders::_1)
 
 	Application::Application() {
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(Application::OnEvent));
 	}
 
 	Application::~Application() {
+	}
+
+	void Application::OnEvent(Event& e) {
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(Application::OnWindowClose));
+
+		FREEZER_CORE_TRACE("{0}", e);
 	}
 
 	void Application::Run() {
@@ -22,5 +29,11 @@ namespace Freezer {
 			m_Window->OnUpdate();
 		};
 	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e) {
+		m_Running = false;
+		return true;
+	}
+
 
 }
--- Freezer/src/Freezer/Application.h
+++ Freezer/src/Freezer/Application.h
@@ -1,7 +1,8 @@
 #pragma once
 
-#include "Core.h"
 #include "Events/Event.h"
+#include "Freezer/Events/ApplicationEvent.h"
+
 #include "Window.h"
 
 namespace Freezer {
@@ -11,7 +12,11 @@ namespace Freezer {
 		virtual ~Application();
 
 		void Run();
+
+		void OnEvent(Event& e);
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
+
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
